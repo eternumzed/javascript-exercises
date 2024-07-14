808a6f6539d7e1fc7c29523985de9a94ec3f453a
eternumzed <jcraft174@gmail.com>
2024-07-14 15:07:07 +0800

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
+    let word = "";
+    for (let i = 1; i <= num; i++) word += string;
+    return word;
 };
+
+const number = Math.floor(Math.random() * 1000)
+
+repeatString("hey", number);
 
 // Do not edit below this line
 module.exports = repeatString;
