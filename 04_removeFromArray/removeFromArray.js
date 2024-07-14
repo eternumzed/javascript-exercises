07d395dc210ffeb449f9194242ddca5c16d1597a
eternumzed <jcraft174@gmail.com>
2024-07-14 19:29:30 +0800

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...element) {
+    newArray = Array.from(array);
+    for (let i = array.length; i--;) {
+        for (let j = 0; j < element.length; j++) {
+            if (array[i] === element[j]) {
+                console.log("Ang tatanggalin", array[i], " at ", element[j])
+                newArray.splice(i, 1);
+            }
+        }
+    }
+    return newArray;
 };
+
+removeFromArray([1, 2, 3, 3, 4], 3, 2,);
 
 // Do not edit below this line
 module.exports = removeFromArray;
