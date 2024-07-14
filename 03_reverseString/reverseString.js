5be2f0e5d9e82a9af6cdab1417929967515b29e0
eternumzed <jcraft174@gmail.com>
2024-07-14 15:22:47 +0800

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let word = "";
+    string = string.split("");
+    for (let i = string.length - 1; i >= 0; i--) word += string[i];
+    return word;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
