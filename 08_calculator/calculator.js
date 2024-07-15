f0b9e765883bfc4179fa5ee43800904c4391749e
eternumzed <jcraft174@gmail.com>
2024-07-15 21:55:28 +0800

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function (num1, num2) {
+  return num1 - num2;
 
 };
 
-const power = function() {
-	
+const sum = function (arr) {
+  if (!arr[0]) return 0;
+  return eval(arr.join("+"));
 };
 
-const factorial = function() {
-	
+const multiply = function (numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
+
+const power = function (num1, num2) {
+  return Math.pow(num1, num2)
+};
+
+const factorial = function (num) {
+  let i = num;
+  if (num === 0) return 1;
+  while (i > 1) {
+    num = num * (i - 1);
+    i--;
+  }
+  return num;
+};
+
+console.log(multiply([2, 4, 6, 8, 10, 12, 14]))
 
 // Do not edit below this line
 module.exports = {
