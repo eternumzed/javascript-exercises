9543651c2685a50f369c753fa1acb70666e6a30d
eternumzed <jcraft174@gmail.com>
2024-07-15 22:55:10 +0800

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let word = string.replaceAll(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let isPalindrome;
+    word = word.split("");
+    let halfLength = Math.floor((word.length / 2).toFixed(1));
+    for (let i = 0; i < halfLength; i++) {
+        if (word[i] === word[word.length - (i + 1)]) {
+            isPalindrome = true;
+        } else isPalindrome = false;
+    }
+    return isPalindrome;
 };
+
+console.log(palindromes("  - Animal loots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
