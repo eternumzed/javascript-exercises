6dfad810597ff10a49b31cfe5282f058c082c29f
eternumzed <jcraft174@gmail.com>
2024-07-15 19:32:57 +0800

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let arr = [];
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (num2 > num1) {
+        for (let i = 0; i < num2 - num1 + 1; i++) arr.push(num1 + i);
+        return arr.reduce((prev, current) => prev + current)
+    } else {
+        for (let i = 0; i < num1 - num2 + 1; i++) arr.push(num2 + i);
+        return arr.reduce((prev, current) => prev + current)
+    }
+}
 
-};
+console.log(sumAll([], 7))
 
 // Do not edit below this line
 module.exports = sumAll;
