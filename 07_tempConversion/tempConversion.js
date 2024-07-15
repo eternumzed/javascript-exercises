119c3c7141c8814499456a60dfa4f6accbd2bb6e
eternumzed <jcraft174@gmail.com>
2024-07-15 20:44:23 +0800

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+
+  let result = (fahrenheit - 32) * 5 / 9;
+  if (String(result).includes(".")) {
+    return Number(result.toFixed(1));
+  }
+  else {
+    return Number(result)
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let result = celsius * 9 / 5 + 32;
+
+  if (String(result).includes(".")) {
+    return Number(result.toFixed(1));
+  }
+  else {
+    return Number(result)
+  }
 };
+
+convertToCelsius(100);
+convertToFahrenheit(2.2);
 
 // Do not edit below this line
 module.exports = {
