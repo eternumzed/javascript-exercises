031c883e8bcbcaa62b7d45e76036bdd258d52674
eternumzed <jcraft174@gmail.com>
2024-07-16 18:58:16 +0800

    Complete exercise 10_fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,23 @@
 const palindromes = function (string) {
-    let word = string.replaceAll(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    let isPalindrome;
-    word = word.split("");
-    let halfLength = Math.floor((word.length / 2).toFixed(1));
-    for (let i = 0; i < halfLength; i++) {
-        if (word[i] === word[word.length - (i + 1)]) {
-            isPalindrome = true;
-        } else isPalindrome = false;
-    }
-    return isPalindrome;
+    const allowedCharacters = `abcdefghijklmnopqrstuvwxyz1234567890`;
+
+    const cleanedVersion = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => allowedCharacters.includes(character))
+        .join('');
+
+    const reversedVersion = cleanedVersion
+        .split('')
+        .reverse()
+        .join('');
+
+    return cleanedVersion === reversedVersion;
+
+
 };
 
-console.log(palindromes("  - Animal loots foliated detail of stool lamina."))
+console.log(palindromes("  - Animal lodots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (nth) {
+    nth = Number(nth);
+    if (nth === 0) return 0;
+    if (nth < 0) return 'OOPS';
+    let prev = 0;
+    let current = 1;
+    for (let i = 1; i < nth; i++) {
+        const helper = current
+        current = prev + current;
+        prev = helper;
+    }
+    return current;
 
 };
+
+console.log(fibonacci("0"))
+
 
 // Do not edit below this line
 module.exports = fibonacci;
