f0feef546958a7726d8fbba5f2e1fd29878e6137
eternumzed <jcraft174@gmail.com>
2024-07-16 19:17:14 +0800

    Complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let arr = []
+    for (let i = 0; i < books.length; i++) {
+        arr.push(books[i].title);
+    }
+    return arr
 };
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name',
+    },
+    {
+        title: 'Book2',
+        author: 'Name2',
+    },
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
